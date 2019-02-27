7d7a2dbc61f97e671a0ff81e5aa7c25a4d64f063
Jackie Lin <jlin00@stuy.edu>
2019-02-26 23:05:00 -0500

    added new code to convert file to 2d array and string

---
+++ ReadFile.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class ReadFile {
+  public static void main(String args[]) throws FileNotFoundException {
+    File text = new File("Maze1.txt");
+
+    //inf stands for the input file
+    Scanner inf = new Scanner(text);
+
+    //stores lines from text file
+    ArrayList<String> lines = new ArrayList<String>();
+
+    //adds each line to text file
+    while(inf.hasNextLine()){
+      String line = inf.nextLine();
+      //System.out.println(line);
+      lines.add(line);
+    }
+
+    int numLines = lines.size(); //number of lines
+    int numChars = lines.get(0).length(); //number of chars in each line
+    char[][] maze = new char[numLines][numChars]; //initializes array
+
+    //adds to array
+    for (int x = 0; x < numLines; x++){
+      for (int y = 0; y < numChars; y++){
+        maze[x][y] = lines.get(x).charAt(y);
+      }
+    }
+
+    //convert array to String
+    String output = "";
+    for (int x = 0; x < numLines; x++){
+      for (int y = 0; y < numChars; y++){
+        output += maze[x][y];
+      }
+      output += "\n";
+    }
+
+    System.out.println(output);
+
+  }
+}
